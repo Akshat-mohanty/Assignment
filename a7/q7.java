302c99537ae1de5cbefb472c8ab4ba2f9cee79c1
Akshat Mohanty <87582409+Akshat-mohanty@users.noreply.github.com>
2024-02-07 16:11:20 +0530

    Add files via upload

---
+++ a7/q7.java
@@ -0,0 +1,12 @@
+package a7;
+import java.util.*;
+class q7
+{
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the decimal number :");
+		String s=Integer.toOctalString(in.nextInt());
+		System.out.println("Octal Value of the entered number is "+s);
+	}
+}
