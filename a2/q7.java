251d7749a75229eab38c35f334226d789cc38e51
Akshat Mohanty <87582409+Akshat-mohanty@users.noreply.github.com>
2024-02-07 16:06:48 +0530

    Add files via upload

---
+++ a2/q7.java
@@ -0,0 +1,13 @@
+package a2;
+import java.util.*;
+class q7
+{
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the side : ");
+		double s=in.nextDouble();
+		double a=3*Math.sqrt(3)*s*s*0.5;
+		System.out.print("The area of the hexagon is "+a);
+	}
+}
